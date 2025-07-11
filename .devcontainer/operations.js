be904161145672731fa5a7aa9c65ea94b793c09e
prabhat93155 <kumarprabhat8130@gmail.com>
2025-07-11 08:01:26 +0000

    Operatores done

---
+++ .devcontainer/operations.js
@@ -0,0 +1,29 @@
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+console.log(2 + 2);
+console.log(3 - 1);
+console.log(2 * 9);
+console.log(4 / 2);
+console.log(4 % 2);
+console.log(2 ** 2);
+
+let str1 = "hello";
+let str2 = "Prabhat";
+let str3 = str1 + str2;
+console.log(str3);
+
+//Important
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log((3 + 2) * 4 % 3);
+
+console.log(+true);
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
