176bb897702eb503e7a6f05fdd7b8b327b988d9b
prabhat93155 <kumarprabhat8130@gmail.com>
2025-07-11 07:21:25 +0000

    data type done

---
+++ .devcontainer/dataTypes.js
@@ -0,0 +1,25 @@
+"use strict" //treat all Js code as never version
+
+// alert("hello"); //we are using nodejs not a browser
+
+console.log(3 + 3);
+console.log("Prabhat");
+
+let name="prabhat";
+let age=21;
+let isLoggedIn=false;
+
+//number =>2 to power 53
+//bigint
+//String=>""
+//bollean=>true/false
+//null => standalone vale
+//undefined =>
+// symbol => unique
+
+
+//Object
+
+console.log(typeof"Prabhat");
+console.log(typeof null);//Object
+console.log(typeof undefined);//undefined
