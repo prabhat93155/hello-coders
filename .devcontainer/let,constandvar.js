b45929482f61391582379bf8a5813674f36e2710
prabhat93155 <kumarprabhat8130@gmail.com>
2025-07-11 06:48:44 +0000

    let,const and var done

---
+++ .devcontainer/let,constandvar.js
@@ -0,0 +1,11 @@
+const accountId=144553;
+let accountEmail="prabhatkumar@gmail.com";
+var accountPassword="12345";
+accountCity="delhi";
+let accoundState;
+accountEmail="hc@gmail.com";
+accountPassword="123";
+accountCity="mumbai";
+
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accoundState]);
