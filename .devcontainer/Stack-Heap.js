37768ff7a8f4b3d55da1d8a4cff4b90f8e66c85d
prabhat93155 <kumarprabhat8130@gmail.com>
2025-07-12 05:29:56 +0000

    Both Done

---
+++ .devcontainer/Stack-Heap.js
@@ -0,0 +1,18 @@
+// stack and Heap
+
+let Myname ="My name is prabhat kumar";
+let anotherName=Myname;
+anotherName="Me Prabhat Kumar";
+console.log(Myname);
+console.log(anotherName);
+
+let user ={
+    email:"user@gmail.com",
+    upi:"upi@ybl",
+}
+
+let user2 = user;
+user2.email="prabhat@gmail.com";
+
+console.log(user.email);
+console.log(user2.email);
