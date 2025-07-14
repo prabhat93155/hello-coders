cc487a6c2f3949235f66e73a8284e9579e44a13f
prabhat93155 <kumarprabhat8130@gmail.com>
2025-07-14 05:12:03 +0000

    Done

---
+++ .devcontainer/Scopelevel_miniHpsting.js
@@ -0,0 +1,29 @@
+function one() {
+    const username = "Youtube"
+    function two() {
+        const webname = "Prabhat"
+        console.log(username);
+    }
+    two()
+}
+// one()
+
+if (true) {
+    const username = "prabhat"
+    if (username === "prabhat") {
+        const website = "youtube"
+        console.log(username + website)
+    }
+}
+
+//++++++++++++++instersting+++++++++++++
+
+function addone(num) {
+    return num + 1
+}
+addone(5)
+
+const addtwo=function(num){
+     return num+2
+}
+addtwo(5)
